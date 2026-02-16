fce5687198759e140aede59374251aab03a046a6
Ranjith9900 <cranjith9900@gmail.com>
2026-02-16 16:29:20 +0530

    chanhe ui 3

--- lib/api.ts
+++ lib/api.ts
@@ -1,14 +1,19 @@
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL!;
-
+const url = process.env.NEXT_PUBLIC_API_URL;
 // health
 export async function getHealth() {
-  const res = await fetch(`${BASE_URL}/health`);
+  const res = await fetch(`${BASE_URL}/health/`);
+
+  if (!res.ok) {
+    throw new Error("Health check failed");
+  }
+
   return res.json();
 }
 
 // get all names
 export async function getNames() {
-  const res = await fetch(`${BASE_URL}/names`, {
+  const res = await fetch(`${BASE_URL}/names/`, {
     cache: "no-store",
   });
   return res.json();
@@ -16,7 +21,7 @@ export async function getNames() {
 
 // add name
 export async function addName(name: string) {
-  const res = await fetch(`${BASE_URL}/names`, {
+  const res = await fetch(`${BASE_URL}/names/`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
